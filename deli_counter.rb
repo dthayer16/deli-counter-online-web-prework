71b3f8073819f52af2d3b05b3eb8d0d2f2bbb966
David Thayer <dthayer16@gmail.com>
2019-03-05 10:49:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+katz_deli = []
+
+def line (number_of_people)
+  if number_of_people == []
+    puts "The line is currently empty"
+  else
+    katz_deli << number_of_people
+    katz_deli.each do |num|
+    puts "The line is currently #{num}."
+    end
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  array.each_with_index.map do |n, i|
+  puts "Welcome, #{name}. You are number #{i+1} in line."
+  end
+end
+
+def now_serving(array)
+  puts "Currently serving #{array[0]}"
+
+  puts "There is nobody waiting to be served!"
+end
