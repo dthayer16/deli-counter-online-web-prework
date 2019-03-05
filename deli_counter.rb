3197c8ed434b46ca550927e1ee9ea899f3b270d0
David Thayer <dthayer16@gmail.com>
2019-03-05 11:50:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array == []
     puts "The line is currently empty."
   else array.each_with_index.map do |name, index|
-    "The line is currently #{index+1}. #{name}"
+    puts "The line is currently #{index+1}. #{name}"
     end
   end
 end
