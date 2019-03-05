7fb3cfa06c73ca3dee08a4c75c97c2580c56c046
David Thayer <dthayer16@gmail.com>
2019-03-05 11:49:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,10 @@
 katz_deli = []
 
 def line(array)
-  array << new_customer
   if array == []
     puts "The line is currently empty."
   else array.each_with_index.map do |name, index|
-    puts "The line is currently #{index+1}. #{name}"
+    "The line is currently #{index+1}. #{name}"
     end
   end
 end
