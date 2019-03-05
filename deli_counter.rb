435a1f96b266d31e60be6d2edf539a456cf6f2f7
David Thayer <dthayer16@gmail.com>
2019-03-05 11:58:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,9 +18,9 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
-  if array ==[]
+  if array == []
     puts "There is nobody waiting to be served!"
-  else
+  else 
     puts "Currently serving #{}."
   end
   array.shift(0)
