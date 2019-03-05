c01ed4dcd2f5fa3c801d39af2789a0e0f21ffc31
David Thayer <dthayer16@gmail.com>
2019-03-05 11:23:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ katz_deli = []
 def line(array)
   if array == []
     puts "The line is currently empty."
-  else array.each_with_index do |name, index|
-    puts "The line is currently #{name, index+1}."
+  else array.each_with_index do |index, name|
+    puts "The line is currently #{index+1, name}."
     end
   end
 end
